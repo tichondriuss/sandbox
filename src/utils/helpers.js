1fed63af415a981734352f43c14bf9da9b83fab9
Boldchingis <bboldchingis@gmail.com>
2026-01-16 22:38:45 +0000

    feat: add error handling and update related files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -83,3 +83,4 @@
 // 2026-01-15 21:42:17 +08 - fix: fix test failure
 // 2026-01-15 23:29:27 +08 - fix: fix memory leak
 // 2026-01-16 20:52:25 +08 - feat: add UI components
+// 2026-01-17 06:38:45 +08 - feat: add error handling
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -77,3 +77,4 @@
 2026-01-16 17:30:35 +08 - related update
 2026-01-17 00:34:00 +08 - related update
 2026-01-17 06:24:44 +08 - related update
+2026-01-17 06:38:45 +08 - related update
